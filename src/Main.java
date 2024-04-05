7fb5ac624db56aa0be3cccf17ead7253bbf63b32
Rakhman <231355@astanait.edu.kz>
2024-04-05 08:51:28 +0500

    2 Task

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,20 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int  fun(int n) {
+    public static int  fun(int n,int[] arr) {
         if (n == 0) {
             return 0;
         }
         else {
-            return n*n+fun(n-1);
+            return arr [n-1] + fun(n-1,arr);
         }
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int num1 = scanner.nextInt();
-        int x = fun(num1);
+        int[] arr = {1,2,3,4,5,6,7,8,9};
+        int num = scanner.nextInt();
+        int x = fun(num,arr);
         System.out.println(x);
 
     }
