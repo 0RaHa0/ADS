58085c21447e29df939bcf221a7dcf56348926c8
Rakhman <231355@astanait.edu.kz>
2024-04-09 22:36:40 +0500

    7 Task

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,39 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void  fun(int n,Scanner scanner) {
-        if (n > 0) {
-            String str = scanner.next();
-            fun(n - 1, scanner);
-            System.out.println(str);
+    public static void  fun(int [][] matrix, int start , int size , int num ) {
+        if (size == 1) {
+            matrix[start][start] = num;
+        }
+        else {
+            int end = start + size -1;
+            for (int i = start; i < end; i++){
+                matrix[start][i] = num++;
+            }
+            for (int i = start; i < end ; i++){
+                matrix[i][end] = num++;
+            }
+            for (int i = end; i > start; i--){
+                matrix[end][i]=num++;
+            }
+            for (int i = end; i>start; i--){
+                matrix[i][start]=num++;
+            }
+            fun(matrix,start+1,size-2,num);
         }
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int num = scanner.nextInt();
-        fun(num,scanner);
+        int n = scanner.nextInt();
+        int[][] matrix = new int[n][n];
+        fun(matrix,0,n,1);
+        for (int[]row:matrix){
+            for (int num:row){
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
 
     }
 }
