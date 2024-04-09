d2fae8b46c12fe943cca8b7fcb02690f9c61550c
Rakhman <231355@astanait.edu.kz>
2024-04-09 23:07:47 +0500

    8 Task

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,28 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
-    public static void  fun(int [][] matrix, int start , int size , int num ) {
-        if (size == 1) {
-            matrix[start][start] = num;
+    public static void  fun(int n, int m, List<Integer> seq) {
+        if (n == 0) {
+            for (int num : seq) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
         }
         else {
-            int end = start + size -1;
-            for (int i = start; i < end; i++){
-                matrix[start][i] = num++;
+            for (int i = 1; i <= m; i++){
+                seq.add(i);
+                fun(n-1,m,seq);
+                seq.remove(seq.size()-1);
             }
-            for (int i = start; i < end ; i++){
-                matrix[i][end] = num++;
-            }
-            for (int i = end; i > start; i--){
-                matrix[end][i]=num++;
-            }
-            for (int i = end; i>start; i--){
-                matrix[i][start]=num++;
-            }
-            fun(matrix,start+1,size-2,num);
         }
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        int[][] matrix = new int[n][n];
-        fun(matrix,0,n,1);
-        for (int[]row:matrix){
-            for (int num:row){
-                System.out.print(num + " ");
-            }
-            System.out.println();
-        }
-
+        int m = scanner.nextInt();
+        fun(n,m,new ArrayList<>());
     }
 }
